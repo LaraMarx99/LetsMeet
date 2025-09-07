b1bd6e338042b30547c07e052aab6d4f6f0d9351
Lara <lara.m14@schule.bremen.de>
2025-09-07 18:59:58 +0000

    Tabelle "interessiert_an" erstellt

--- results/create_letsmeet.sql
+++ results/create_letsmeet.sql
@@ -1,15 +1,16 @@
-drop table if exists Geschlecht;
 drop table if exists Hobbys;
 drop table if exists Nutzer;
+drop table if exists Geschlecht;
+
 
 CREATE TABLE Geschlecht(
     ID INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    Geschlechtsindentität VARCHAR(100)
+    Geschlechtsindentität VARCHAR(100) UNIQUE
 );
 
 CREATE TABLE Hobbys(
     ID INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
-    Hobbys VARCHAR(100)
+    Hobbys VARCHAR(100) UNIQUE
 );
 
 CREATE TABLE Nutzer(
@@ -18,7 +19,7 @@ CREATE TABLE Nutzer(
     Vorname VARCHAR(100),
     Geburtsdatum DATE,
     Telefonnummer INT,
-    EMail VARCHAR(100),
+    EMail VARCHAR(100) UNIQUE,
     Straße VARCHAR(100),
     Hausnummer VARCHAR(100),
     PLZ INT,
@@ -27,3 +28,10 @@ CREATE TABLE Nutzer(
     UpdatedAt TIMESTAMP,
     CreatedAt TIMESTAMP
 );
+
+CREATE TABLE interessiert_an(
+    Nutzer_ID INT REFERENCES Nutzer(id),
+    Interesse INT REFERENCES Geschlecht(id)
+);
+
+
